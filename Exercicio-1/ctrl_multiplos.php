2634737ed3d829b8ecf6ba586f1f09bae6a18a82
guilhermehifi2016 <guilhermepereiraramos@gmail.com>
2016-11-04 18:44:57 -0200

    Update ctrl_multiplos.php

--- Exercicio-1/ctrl_multiplos.php
+++ Exercicio-1/ctrl_multiplos.php
@@ -1,1 +1,21 @@
-Solu
+<?php
+
+function multiplo($i = 1)
+{
+
+    for ($i = 1; $i <= 100; $i++) {
+
+        if (($i % 3 == 0) and ( $i % 5 == 0)) {
+            echo "FizzBuzz</br>";
+        } elseif ($i % 5 == 0) {
+            echo "Buzz</br>";
+        } elseif ($i % 3 == 0) {
+            echo "Fizz</br>";
+        } else {
+            echo $i;
+        }
+    }
+}
+$escrever = multiplo();
+
+echo $escrever;
